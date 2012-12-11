5be6f315ada1d064a75f5ac557dbc10731b77bf3
Aaron Jensen <aaronjensen@gmail.com>
2012-12-10 22:35:13 -0800

    Initial template

--- lib/guard-zeus-server.rb
+++ lib/guard-zeus-server.rb
@@ -1,9 +1,2 @@
-require "guard-zeus-server/version"
-
-module Guard
-  module Zeus
-    module Server
-      # Your code goes here...
-    end
-  end
-end
+require "guard/zeus_server/version"
+require "guard/zeus_server"
